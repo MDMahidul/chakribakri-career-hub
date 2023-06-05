f6e0bb290fa03c04c54a4840d971e29c660ac09e
MDMahidul <mmbmahidul007@gmail.com>
2023-06-05 20:36:36 +0600

    added filter function to show remote or onsite job in the applied jobs page

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import HeaderBanner from "../HeaderBanner/HeaderBanner";
 import AppliedCart from "../AppliedCart/AppliedCart";
 import { CartContext } from "../../App";
@@ -7,11 +7,31 @@ import { useNavigate } from "react-router-dom";
 
 const AppliedJobs = () => {
   const [cart, setCart] = useContext(CartContext);
+  const [originalCart, setOriginalCart] = useState(cart);
   const navigate = useNavigate();
+
+  const handleOnsiteJobs = () => {
+    const onsiteJobs = originalCart.filter((job) => job.category === "Onsite");
+    setCart(onsiteJobs);
+  };
+
+  const handleRemoteJobs = () => {
+    const remoteJobs = originalCart.filter((job) => job.category === "Remote");
+    setCart(remoteJobs);
+  };
+
   return (
     <div>
       <HeaderBanner>Applied Jobs</HeaderBanner>
       <div className="container mx-auto">
+        <div className="d-flex justify-content-end gap-4 mb-3">
+          <Button onClick={() => handleRemoteJobs()} className="my-btn">
+            Remote Jobs
+          </Button>
+          <Button onClick={() => handleOnsiteJobs()} className="my-btn">
+            Onsite Jobs
+          </Button>
+        </div>
         {cart.map((job) => (
           <AppliedCart key={job.id} job={job}></AppliedCart>
         ))}
